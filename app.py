e502f8907f5042f7218ba2232878d8c86b0f44e9
Kaleb Asefa <Kalebasefa0@gmail.com>
2026-06-16 15:13:07 +0300

    Refactor country data fetching and improve error handling in API requests

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import streamlit as st
-from main import data
+from main import country_request
 
 st.title("Countries API Data")
 
-country_name = data["data"]["objects"][0]["names"]["common"]
-flag_emoji = data["data"]["objects"][0]["flag"]["emoji"]
-capital_city = data["data"]["objects"][0]["capitals"][0]['name']
+st.text_input("Enter a country name", key="country_name")
 
-st.write(f'you choose a country: {country_name} {flag_emoji}')
-st.write(f'capital city of {country_name} is: {capital_city}')
+if st.button("Get Country Data"):
+    country_name = st.session_state.country_name
+    data = country_request(country_name)
 
-st.json((data['data']['objects']))
-st.write(len((data['data']['objects'])))
+    if isinstance(data, str):
+        st.write(data)
+    else:
+
+        country_name = data["data"]["objects"][0]["names"]["common"]
+        flag_emoji = data["data"]["objects"][0]["flag"]["emoji"]
+        capital_city = data["data"]["objects"][0]["capitals"][0]['name']
+
+        st.write(f'you choose a country: {country_name} {flag_emoji}')
+        st.write(f'capital city of {country_name} is: {capital_city}')
+
+        st.write((data))
+        st.write(len((data['data']['objects'])))
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import json
+from requests.exceptions import HTTPError, ConnectionError, Timeout
 
 from dotenv import load_dotenv
 import os
@@ -8,12 +8,31 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-params = {'q':'ethi'}
 
-response = requests.get(
-  'https://api.restcountries.com/countries/v5',
-  headers={'Authorization': f'Bearer {API_KEY}'}, params=params
-)
-data = response.json()
-print(json.dumps(data, indent=2))
-print(response.status_code)
+def country_request(country_name):
+  params = {'q': country_name}
+  try:
+    response = requests.get(
+      'https://api.restcountries.com/countries/v5',
+      headers={'Authorization': f'Bearer {API_KEY}'}, params=params
+      )
+    response.raise_for_status()
+
+    data = response.json()
+
+    if not data['data']['objects']:
+      return f'No data found for country: {country_name}'
+    
+    else:
+      return data
+
+  except HTTPError as http_err:
+    return f"HTTP error occurred: {http_err}"
+
+  except ConnectionError as conn_err:
+    return f"Connection error occurred: {conn_err}"
+
+  except Timeout as timeout_err:
+    return f"Timeout error occurred: {timeout_err}"
+
+
